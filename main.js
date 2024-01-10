ba272e3b6f5e9cb0a95b5aed52d482fcab372aaf
(Deivide <(deividemaciel2@gmail.com)>
2024-01-10 15:38:38 -0300

    Testando aceleração do eixo X

--- main.js
+++ main.js
@@ -1,14 +1,20 @@
-const updateDisplay = value => {
-    const display = document.querySelector('.display p')
-    display.innerHTML = value
+const updateDisplay = (value) => {
+    if(!value) value = 0
+    const display = document.querySelector('.display #acceleration')
+    const lastAcceleration = document.querySelector('.display #last-acceleration')
+    let highestRecordedAcceleration = 0
+
+    if(value > highestRecordedAcceleration) highestRecordedAcceleration = value
+
+    display.innerHTML = `Aceleração no exio X: ${value.toFixed(2)} m/s²`
+    lastAcceleration.innerHTML = `Maior aceleração registrada: ${highestRecordedAcceleration} m/s²`
 }
 
 window.onload = () => {
     if(window.DeviceMotionEvent) {
         window.addEventListener('devicemotion', event => {
             const acceleration = event.acceleration.x
-            console.log(acceleration)
-            updateDisplay('Aceleração no eixo X: ' + acceleration.toFixed(3))
+            updateDisplay(acceleration)
         })
 
     } else console.log('Seu navegador não suporta a API de orientação do dispositivo.')    
